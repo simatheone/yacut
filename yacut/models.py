4c22b9dcbabfa4e64b6fb5674a285de8693461fd
simatheone <samone07@mail.ru>
2022-09-03 19:56:24 +0300

    Refactor to_dict method. Add methods: from_dict, is_short_link_exists, get_unique_short_id, is_valid_short_link

--- yacut/models.py
+++ yacut/models.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+from random import choice
 
-from yacut import db
+from flask import url_for
+
+from . import db
+from settings import SYMBOLS_LENGTH, USER_INPUT_LIMIT, VALID_SYMBOLS
 
 
 class URL_map(db.Model):
@@ -11,8 +15,32 @@ class URL_map(db.Model):
 
     def to_dict(self):
         return dict(
-            id = self.id,
-            original = self.original,
-            short = self.short,
-            timestamp = self.timestamp
+            url=self.original,
+            short_link=url_for(
+                'short_link_view', id=self.short, _external=True
+            )
         )
+
+    def from_dict(self, data):
+        setattr(self, 'original', data['url'])
+        setattr(self, 'short', data['custom_id'])
+
+    def is_short_link_exists(self, custom_id):
+        return bool(self.query.filter_by(short=custom_id).first())
+
+    def get_unique_short_id(self):
+        result_short_id = ''
+        while len(result_short_id) != SYMBOLS_LENGTH:
+            result_short_id += choice(VALID_SYMBOLS)
+
+        if not self.is_short_link_exists(result_short_id):
+            return result_short_id
+        return self.get_unique_short_id()
+
+    def is_valid_short_link(self, short_link):
+        if len(short_link) > USER_INPUT_LIMIT:
+            return False
+        for value in short_link:
+            if value not in VALID_SYMBOLS:
+                return False
+        return True
